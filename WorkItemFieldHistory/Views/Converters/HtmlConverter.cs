e4616138f90e90b4b7e30b18de5997560026546e
Fernando Oliveira <fernando@fholiveira.com>
2014-10-14 14:21:13 -0300

    Fix new line replace

--- WorkItemFieldHistory/Views/Converters/HtmlConverter.cs
+++ WorkItemFieldHistory/Views/Converters/HtmlConverter.cs
@@ -9,18 +9,19 @@ using System.Windows.Data;
 
 namespace Lambda3.WorkItemFieldHistory.Views.Converters
 {
-    class HtmlConverter: IValueConverter
-
+    class HtmlConverter : IValueConverter
     {
+        private const char newLine = '\n';
+        private const char space = ' ';
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value==null)
-            {
+            if (value == null)
                 return string.Empty;
-            }
-            string s = value.ToString();
-            return s.Replace(Environment.NewLine, " ");
+
+            return value.ToString().Replace(newLine, space);
         }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
