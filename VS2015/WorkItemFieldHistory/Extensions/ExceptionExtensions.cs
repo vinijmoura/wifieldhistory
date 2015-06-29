46c315dd1fd83ff7b078d65cdb3dc7ef7b9a2368
Vinicius Moura <vinijmoura@hotmail.com>
2015-06-29 18:12:19 -0300

    VS 2015 version

---
+++ VS2015/WorkItemFieldHistory/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace Lambda3.WorkItemFieldHistory.Extensions
+{
+    static class ExceptionExtensions
+    {
+        public static void Show(this Exception exception, string title = "Work Item Field History")
+        {
+            MessageBox.Show(exception.Message,
+                            title,
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error,
+                            MessageBoxResult.OK,
+                            MessageBoxOptions.DefaultDesktopOnly | MessageBoxOptions.DefaultDesktopOnly);
+        }
+    }
+}
---
+++ VS2015/WorkItemFieldHistory/Models/Revision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Lambda3.WorkItemFieldHistory.Models
+{
+    public class Revision
+    {
+        public int Index { get; set; }
+        public IDictionary<string, Field> Fields { get; set; }
+
+        public FieldAtRevision GetField(string fieldName)
+        {
+            return new FieldAtRevision
+            {
+                RevisionNumber = Index + 1,
+                RevisedBy = GetFieldValue("Changed By"),
+                RevisionDate = GetFieldValue("Changed Date"),
+                NewValue = GetFieldValue(fieldName),
+                OldValue = GetFieldOriginalValue(fieldName),
+                FieldName = fieldName,
+                ReferenceName = Fields[fieldName].ReferenceName,
+                FieldType = Fields[fieldName].Type
+            };
+        }
+
+        private string GetFieldValue(string fieldName)
+        {
+            return (Fields[fieldName].Value ?? string.Empty).ToString();
+        }
+
+        private string GetFieldOriginalValue(string fieldName)
+        {
+            return (Fields[fieldName].OriginalValue ?? string.Empty).ToString();
+        }
+    }
+}
